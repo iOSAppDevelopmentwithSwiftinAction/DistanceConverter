35db3fe354c3ad52327b54983fa326040272ec26
Craig Grummitt <craiggrummitt@gmail.com>
2017-03-13 09:48:42 -0500

    Changed Distance to struct, made var

--- DistanceConverter/ViewController.swift
+++ DistanceConverter/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  let distance = Distance(miles: 1000)
+  var distance = Distance(miles: 1000)
   
   @IBOutlet weak var milesTextField: UITextField!
   @IBOutlet weak var kmTextField: UITextField!
